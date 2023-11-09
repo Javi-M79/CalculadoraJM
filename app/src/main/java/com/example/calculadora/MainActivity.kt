9e8c924c14f07f6ca73c45040fded573c9c47e33
Silx79 <javier.mira46@gmail.com>
2023-11-09 17:38:58 +0100

    Vinculacion de parte grafica. Implementacion de interface OnclickListener.

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
+import com.example.calculadora.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , OnClickListener{ //IMPLEMENTAMOS INTERFACE OnClickListener y creamos el metodo mas abajo.
 
-    //CREAMOS LAS VARIABLES
-
+    /*
+    CREAMOS LAS VARIABLES
     private lateinit var texto_pantalla: TextView
     private lateinit var boton_reset: Button
     private lateinit var boton_borrar: Button
@@ -27,16 +30,45 @@ class MainActivity : AppCompatActivity() {
     private lateinit var boton_resta: Button
     private lateinit var boton_historial: Button
     private lateinit var boton_cero: Button
-    private lateinit var boton_igual: Button
+    private lateinit var boton_igual: Button*/
 
 
-    //Importar fichero grafico
+//    Vincular el fichero grafico. Implementar ViewBinding en build.gradle(module:app) con este codigo antes de las dependencias
+//     viewBinding {
+//            enable = true
+//        }
 
+    private lateinit var binding: ActivityMainBinding //En esta variable creamos el fichero grafico de la mainActivity que es de tipo ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //UNIMOS LA PARTE GRAFICA CON PARTE LOGICA
-        setContentView(R.layout.activity_main)
+        //Cogemos el fichero y lo rellenamos con el metodo "inflate"
+        binding =
+            ActivityMainBinding.inflate(layoutInflater)//Nombre de la clase + metodo = (variable LayoutInflater)
+        setContentView(binding.root)
+
+        /*TODO EL CODIGO DE ARRIBA SE DEBE REPETIR POR NORMA
+        -  Crear variable para el ActivityMain de tipo ActivityMainBinding
+        - Rellenar fichero
+        -  Usar la clase serContentView con la raiz de la variable Binding como parametro. Con root le estamos diciendo que mire lo mas arriba posible. Normalmente desde el linearLayout, etc..
+
+        */
+
+        //Si usamos binding. nos apareceran todos los elementos que hemos creado en la parte grafica.
+
+        binding.botonBorrar.setOnClickListener(this)//Con this, llamamos a la funcion de la interface.
+        binding.botonCero
+        binding.botonDividir
+
+
+        //
+
+    }
+
+
+    //Metodo de la Interface OnClickListener.
+    override fun onClick(v: View?) {
+
     }
 }
