24ef24310d1e0943ec506e281dcb2f4ca7558592
Silx79 <javier.mira46@gmail.com>
2023-11-13 19:44:15 +0100

    Implementados Toast en botones de operacion

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.calculadora.databinding.ActivityMainBinding
 
 
@@ -14,37 +15,39 @@ import com.example.calculadora.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), OnClickListener {
 
 
-//    Vincular el fichero grafico. Implementar ViewBinding en build.gradle(module:app) con este codigo antes de las dependencias
-//     viewBinding {
-//            enable = true
-//        }
+    /*    Vincular el fichero grafico. Implementar ViewBinding en build.gradle(module:app) con este codigo antes de las dependencias
+             viewBinding {
+                enable = true
+                }*/
 
     private lateinit var binding: ActivityMainBinding //En esta variable creamos el fichero grafico de la mainActivity que es de tipo ActivityMainBinding
-
-    //    private var textoSecudario = ""//Probar a meter los valores introducidos en esta pantalla y mostrar el resultado antes del igual como en la calculadora samsung.
+    private var pantallaSecudario = ""
     private var pantalla = ""
     private var op1 = 0
     private var op2 = 0
-    private var resultado = 0
+    private var resultado =
+        0 //Probar con String para ver si se puede resetear al pulsar un numero despues de mostrarlo.
     private var operacion: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        //Cogemos el fichero y lo rellenamos con el metodo "inflate"//
 
-        //Cogemos el fichero y lo rellenamos con el metodo "inflate"
         binding =
             ActivityMainBinding.inflate(layoutInflater)//Nombre de la clase + metodo = (variable LayoutInflater)
         setContentView(binding.root)
 
         /*TODO EL CODIGO DE ARRIBA SE DEBE REPETIR POR NORMA
-        -  Crear variable para el binding de tipo ActivityMainBinding
-        - Rellenar fichero
-        -  Usar la clase serContentView con la raiz de la variable Binding como parametro. Con root le estamos diciendo que mire lo mas arriba posible. Normalmente desde el linearLayout, etc..
+        RESUMEN:
+        -  Crear variable para el binding de tipo ActivityMainBinding: private lateinit var binding: ActivityMainBinding
+        - Rellenar fichero: binding = ActivityMainBinding.inflate(layoutInflater)
+        -  Usar la clase serContentView con la raiz de la variable Binding como parametro. Con root le estamos diciendo que mire lo mas arriba posible del archivo grafico. Normalmente desde el linearLayout, etc..
 
         */
 
-        //Si usamos binding. nos apareceran todos los elementos que hemos creado en la parte grafica.
+        //Si usamos binding.nombredelelemento en el xml, nos apareceran todos los elementos que hemos creado en la parte grafica.
 
 
         binding.botonSuma.setOnClickListener(this)
@@ -75,6 +78,10 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     //Metodo de la Interface OnClickListener.
     override fun onClick(v: View?) {
 
+        /*IMPLEMENTAR RESETEO AL MOSTRAR RESULTADO PARA EMPEZAR DE NUEVO Y NO CONCATERNAR NUEVOS VALORES AL RESULTADO.
+          IF ELSE?
+          Trabajar con al variable resultado. Resetearla?
+      */
         when (v?.id) {
 
             binding.botonCero.id -> pantalla += "0"
@@ -92,34 +99,68 @@ class MainActivity : AppCompatActivity(), OnClickListener {
             //EVENTOS EN BOTONES DE OPERACION//
 
             binding.botonSuma.id -> {
-                op1 = pantalla.toInt()
-                pantalla = ""
-                operacion = "suma"
+
+                if (pantalla.equals("")) {
+
+                    Toast.makeText(this, "Accion no permitida", Toast.LENGTH_SHORT).show()
+                } else {
+                    op1 = pantalla.toInt()
+                    pantalla = ""
+                    pantallaSecudario = "+"
+                    operacion = "suma"
+                }
             }
 
             binding.botonResta.id -> {
-                op1 = pantalla.toInt()
-                pantalla = ""
-                operacion = "resta"
+                if (pantalla.equals("")) {
+
+                    Toast.makeText(this, "Accion no permitida", Toast.LENGTH_SHORT).show()
+                } else {
+                    op1 = pantalla.toInt()
+                    pantalla = ""
+                    pantallaSecudario = "-"
+                    operacion = "resta"
+                }
             }
 
             binding.botonMultipilcar.id -> {
-                op1 = pantalla.toInt()
-                pantalla = ""
-                operacion = "multiplicacion"
+                if (pantalla.equals("")) {
+
+                    Toast.makeText(this, "Accion no permitida", Toast.LENGTH_SHORT).show()
+                } else {
+
+                    op1 = pantalla.toInt()
+                    pantalla = ""
+                    pantallaSecudario = "x"
+                    operacion = "multiplicacion"
+                }
             }
 
             binding.botonDividir.id -> {
-                op1 = pantalla.toInt()
-                pantalla = ""
-                operacion = "division"
+                if (pantalla.equals("")) {
+
+                    Toast.makeText(this, "Accion no permitida", Toast.LENGTH_SHORT).show()
+                } else {
+                    op1 = pantalla.toInt()
+                    pantalla = ""
+                    pantallaSecudario = "/"
+                    operacion = "division"
+                }
             }
+            /*DECIDIR SU USO.
+             SI MUESTRA SIMBOLO EN SECUNDARIA O MUESTRA EL RESULTADO DE LA OPERACION AL PULSARLO*/
 
             binding.botonPorcentaje.id -> {
 
-                op1 = pantalla.toInt()
-                pantalla = ""
-                operacion = "porcentaje"
+                if (pantalla.equals("")) {
+
+                    Toast.makeText(this, "Accion no permitida", Toast.LENGTH_SHORT).show()
+                } else {
+                    op1 = pantalla.toInt()
+                    pantalla = ""
+                    pantallaSecudario = "%"
+                    operacion = "porcentaje"
+                }
             }
 
             binding.botonIgual.id -> {
@@ -127,10 +168,12 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 when (operacion) {
 
                     "suma" -> {
+
                         op2 = pantalla.toInt()
                         resultado = op1 + op2
                         pantalla = resultado.toString()
                         operacion = ""
+
                     }
 
                     "resta" -> {
@@ -152,19 +195,23 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
                     "division" -> {
 
+
                         op2 = pantalla.toInt() //
                         //Evitar la division por cero.
                         if (op2 != 0) {
                             resultado = op1 / op2
                             pantalla = resultado.toString()
                             operacion = ""
+
                         } else {
-                            pantalla = "Error"
+//
+                            Toast.makeText(this, "Division por 0 no permitida", Toast.LENGTH_SHORT)
+                                .show()
                         }
 
 
                     }
-                        //REVISAR OPERACION. DECIDIR SI USARLA AL PULSAR IGUAL O CUANDO PULSE EL BOTON PORCENTAJE COMO EN LA CALCULADORA.
+                    //REVISAR OPERACION. DECIDIR SI USARLA AL PULSAR IGUAL O CUANDO PULSE EL BOTON PORCENTAJE COMO EN LA CALCULADORA.
                     "porcentaje" -> {
 
                         op2 = pantalla.toInt() //
@@ -181,6 +228,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
         }
         binding.textoPantalla.text = pantalla
+        binding.textoSecundario.text = pantallaSecudario
     }
 
 }
